51e77f711eea83c71a435978987c20111438f200
ansonWang0125 <mglcorhfks@gmail.com>
2023-02-03 00:29:20 +0800

    edit server

--- Service/src/server.js
+++ Service/src/server.js
@@ -7,8 +7,13 @@ const db = require('./Model')
 const userRoutes = require('./Routes/userRoutes')
 const articleRoutes = require('./Routes/articleRoutes')
 const sessionRouter = require('./Routes/session.routes');
+const path = require('path')
+const { dirname } = require('path')
 
-const PORT = process.env.PORT ||8080
+// define routes
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const PORT = process.env.PORT ||4000
 
 const app = express();
 
@@ -21,8 +26,14 @@ db.sequelize.sync().then(() => {    //drop table if exists
     console.log("db has been sync")
 })
 
+
 app.use('/api/users', userRoutes)
 app.use('/api/article', articleRoutes)
 app.use('/api/sessions', sessionRouter);
+
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.get("/*", (_, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`API is runing on ${PORT}`));
