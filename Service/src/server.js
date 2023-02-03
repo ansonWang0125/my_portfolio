d5468e99b12c8eb7ff40f30778348a8b9a8129b1
ansonWang0125 <mglcorhfks@gmail.com>
2023-02-03 18:27:07 +0800

    cors

--- Service/src/server.js
+++ Service/src/server.js
@@ -14,24 +14,13 @@ const PORT = process.env.PORT ||10000
 
 const app = express();
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
+app.get('/*', function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://post-articles.onrender.com/*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
+  res.send('cors problem fixed:)');
 });
 
 
