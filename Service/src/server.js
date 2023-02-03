75bb017a0799f22eea5dbeda266595ce457d9511
ansonWang0125 <mglcorhfks@gmail.com>
2023-02-03 17:33:21 +0800

    cors

--- Service/src/server.js
+++ Service/src/server.js
@@ -15,37 +15,16 @@ const PORT = process.env.PORT ||10000
 
 const app = express();
 
-// const corsOptions ={
-//   origin:'https://post-articles.onrender.com', 
-//   credentials:true,            //access-control-allow-credentials:true
-//   optionSuccessStatus:200,
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-// }
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors());
-
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors(corsOptions));
 
 db.sequelize.sync().then(() => {    //drop table if exists
     console.log("db has been sync")
