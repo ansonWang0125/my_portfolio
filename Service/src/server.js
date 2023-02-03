70b5de20fbd0aa40d8cdb4b59cd2858f6b71fd24
ansonWang0125 <mglcorhfks@gmail.com>
2023-02-03 18:34:03 +0800

    cors

--- Service/src/server.js
+++ Service/src/server.js
@@ -20,7 +20,6 @@ app.get('/*', function(req, res) {
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
   res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
-  res.send('cors problem fixed:)');
 });
 
 
