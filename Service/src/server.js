5454c66a1f7e13a963d943f280dd56e5ed0987e9
ansonWang0125 <mglcorhfks@gmail.com>
2023-02-03 20:07:40 +0800

    test

--- Service/src/server.js
+++ Service/src/server.js
@@ -30,7 +30,14 @@ app.use('/api/article', articleRoutes)
 app.use('/api/sessions', sessionRouter);
 
 
-app.use(express.static(path.join(__dirname, "..", "..", "UI", "build")));
+app.use(express.static(path.join(__dirname, "..", "..", "UI", "build"),{
+  setHeaders: (res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  }
+}));
 app.get("/*", (_, res) => {
   res.sendFile(path.join(__dirname,"..","..", "UI", "build", "index.html"));
 });
