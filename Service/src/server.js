5fdc700a31a995b793d08352e85f9f69eb08f874
ansonWang0125 <b09705044@ntu.edu.tw>
2023-04-29 15:25:47 +0800

    data

--- Service/src/server.js
+++ Service/src/server.js
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({ extended: false, limit: '50mb' }))
 
-db.sequelize.sync({force: true}).then(() => {    //drop table if exists
+db.sequelize.sync().then(() => {    //drop table if exists
     console.log("db has been sync")
 })
 
